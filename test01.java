3bfde6ff6c2cf7e6989bf9185188c753c6a7383a
jifenghy <duqingsong.routon@gmail.com>
2014-04-09 10:20:39 +0800

    333

--- test01.java
+++ test01.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-class UcpSend01
+class UdpSend01
 {
 	public static void main(String[] args) throws Exception
 	{
@@ -12,6 +12,19 @@ class UcpSend01
 		DatagramPacket dp = new DatagramPacket(buf,buf.length
 				,InetAddress.getByAddress(new byte[]{127,0,0,1}),10003);
 		ds.send(dp);
+		ds.close();
+	}
+}
+
+class UdpRece01
+{
+	public static void main(String[] args) throws Exception
+	{
+		DatagramSocket ds = new DatagramSocket(10003);
+		byte[] buf = new byte[1024];
+		DatagramPacket dp = new DatagramPacket(buf,buf.length);
+		System.out.println(new String(buf,0,dp.getLength()));
+		ds.receive(dp);
 		ds.close();
 	}
 }
